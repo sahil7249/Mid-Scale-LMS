016d125754764b73d30e548d31c9f1f40efa26bc
sahil7249 <pathansahil7249@gmail.com>
2025-07-25 11:29:38 +0530

    add user student and instructor model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student','Student'),
+        ('instructor','Instructor'),
+        ('admin','Admin'),
+    )
+
+    role = models.CharField(max_length=12,choices=ROLE_CHOICES)
+    @property
+    def isStudent(self):
+        return self.role == 'student'
+    
+    @property
+    def isInstructor(self):
+        return self.role == 'instructor'
+
+    def __str__(self):
+        return self.get_full_name()
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='student')
+
+    def __str__(self):
+        return self.user.get_full_name()
+    
+class Instructor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='instructor')
+    bio = models.TextField(blank=True,null=True)
+    expertise = models.CharField(max_length=255)
+    is_approved = models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.user.get_full_name()
