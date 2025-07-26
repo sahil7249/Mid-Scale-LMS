66b951bd2e62a0c04114ff2dfdf7f86deda0a220
sahil7249 <pathansahil7249@gmail.com>
2025-07-26 23:40:01 +0530

    add : django debugger

--- backend/LMS_HOME/urls.py
+++ backend/LMS_HOME/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('users.urls'))
-]
+]  + debug_toolbar_urls()
