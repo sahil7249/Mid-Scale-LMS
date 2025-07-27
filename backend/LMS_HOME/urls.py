701a0078252df12d991da3cc8bd2b6667c2d8204
sahil7249 <pathansahil7249@gmail.com>
2025-07-27 23:07:37 +0530

    add : simplejwt

--- backend/LMS_HOME/urls.py
+++ backend/LMS_HOME/urls.py
@@ -18,8 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/',include('users.urls'))
+    path('user/',include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]  + debug_toolbar_urls()
