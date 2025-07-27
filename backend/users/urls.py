6d021ff9bebccd44f0515e6eb388cbc2d2071186
sahil7249 <pathansahil7249@gmail.com>
2025-07-27 23:21:47 +0530

    add : UserLoginView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserView,UserView
+from .views import RegisterUserView,UserView,UserLoginView
 
 urlpatterns = [
     path('register/',RegisterUserView.as_view(),name="register_view"),
-    path('details/<int:pk>/',UserView.as_view(),name="user_view")
+    path('details/<int:pk>/',UserView.as_view(),name="user_view"),
+    path('login/',UserLoginView.as_view(),name='login_view')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,11 @@
 from .serializer import UserSerialzer
 from .models import User
+from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterUserView(APIView):
 
@@ -12,6 +14,21 @@ class RegisterUserView(APIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response(UserSerialzer(user).data,status=status.HTTP_200_OK)
+    
+class UserLoginView(APIView):
+
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            refresh_token = RefreshToken()
+            return Response({
+                'username':user.username,
+                'role':user.role,
+                'refresh':str(refresh_token),
+                'access':str(refresh_token.access_token)
+            })
     
 class UserView(APIView):
     def get(self,request,pk=None):
