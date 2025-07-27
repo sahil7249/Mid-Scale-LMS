6798a67a2b74a884b81d888374b60961016abaa5
sahil7249 <pathansahil7249@gmail.com>
2025-07-27 23:08:37 +0530

    Revert "add : simplejwt"
    This reverts commit 701a0078252df12d991da3cc8bd2b6667c2d8204.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView,UserView,UserLoginView
+from .views import RegisterUserView,UserView
 
 urlpatterns = [
     path('register/',RegisterUserView.as_view(),name="register_view"),
-    path('details/<int:pk>/',UserView.as_view(),name="user_view"),
-    path('login/',UserLoginView.as_view(),name="login_view")
+    path('details/<int:pk>/',UserView.as_view(),name="user_view")
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,41 +1,19 @@
 from .serializer import UserSerialzer
 from .models import User
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterUserView(APIView):
+
     def post(self,request):
         serializer = UserSerialzer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response(UserSerialzer(user).data,status=status.HTTP_200_OK)
-
-class UserLoginView(APIView):
-    def post(self,request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username,password=password)
-        if user is not None :
-            refresh_token = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh_token),
-                'access': str(refresh_token.access_token),
-                'role':user.role,
-                'username':user.username
-            })
-        else:
-            return Response({"detail":"Invalid credentials"},status=status.HTTP_404_NOT_FOUND)
-
-        
-
+    
 class UserView(APIView):
-    permission_classes = [IsAuthenticated]
-
     def get(self,request,pk=None):
         user = User.objects.get(pk=pk)
         serializer = UserSerialzer(user)
